9984f8d9f1af144c68e21c5ca6ef7bca69b30e08
kmssankar <s.sankarganesh@hotmail.com>
2018-06-23 22:33:11 +0530

    Initial Commit - Sankar

---
+++ JavaCollections/src/org/javacollections/list/implementations/ListMain.java
@@ -0,0 +1,10 @@
+package org.javacollections.list.implementations;
+
+public class ListMain {
+  
+	public static void main(String Args[])
+	{
+		
+		
+	}
+}
---
+++ JavaCollections/src/org/javacollections/list/implementations/User.java
@@ -0,0 +1,18 @@
+package org.javacollections.list.implementations;
+
+public class User {
+    private int id;
+	private String name;
+	public int getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+    public void setName(String name) {
+		this.name = name;
+	}   
+}
