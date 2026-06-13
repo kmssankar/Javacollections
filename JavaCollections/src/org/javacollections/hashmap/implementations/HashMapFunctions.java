3279dc333350c0ad9954e896c52a9c7396b3c6bf
Sankar Ganesh <s.sankarganesh@hotmail.com>
2026-06-14 01:08:55 +0530

    updated overriding hashcode and equals for Hashmap

--- JavaCollections/src/org/javacollections/hashmap/implementations/HashMapFunctions.java
+++ JavaCollections/src/org/javacollections/hashmap/implementations/HashMapFunctions.java
@@ -2,7 +2,6 @@ package org.javacollections.hashmap.implementations;
 
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -11,7 +10,7 @@ import java.util.Set;
 public class HashMapFunctions {
 
 	public void AddElement(HashMap<Integer, Shares> InputMap, Shares Sh) {
-		InputMap.put(Sh.shareId, Sh);
+		InputMap.put(Sh.getShareId(), Sh);
 	}
 
 	public void showElements(HashMap<Integer, Shares> InputMap, String Msg) {
@@ -22,7 +21,6 @@ public class HashMapFunctions {
 			Entry<Integer, Shares> Entries = Itr.next();
 			System.out.println(" Key : " + Entries.getKey() + " Value :  " + Entries.getValue());
 		}
-
 	}
 
 	public void showElements(HashMap<Integer, Shares> InputMap)
@@ -34,19 +32,16 @@ public class HashMapFunctions {
 			System.out.println(MapEntries.getKey() + " Value : " + MapEntries.getValue());
 		}
 	}
-	
 	public void ContainKey(HashMap<Integer, Shares> InputMap , Integer Key)
 	{
 		String Msg = (InputMap.containsKey(Key))?"Key Found ":"Key Not Found ";
 		System.out.println(Msg +  Key);
 	}
-	
 	public void ContainValue(HashMap<Integer, Shares> InputMap , Shares SearchShare)
 	{
 		String Msg = (InputMap.containsValue(SearchShare))?"Key Found ":"Key Not Found ";
 		System.out.println(Msg +  SearchShare);
 	}
-	
 	public void RemoveValue(HashMap<Integer, Shares> InputMap , Shares RemoveShare)
 	{
 		String Msg = (InputMap.remove(RemoveShare.getShareId())!=null)?"Value Removed ":"Value Not Found ";
--- JavaCollections/src/org/javacollections/hashmap/implementations/Shares.java
+++ JavaCollections/src/org/javacollections/hashmap/implementations/Shares.java
@@ -1,11 +1,11 @@
 package org.javacollections.hashmap.implementations;
 
+import java.util.Objects;
 public class Shares {
 
-	int shareId;
-	String tickker;
-	String companyDesc;
-	
+	private int shareId;
+	private String tickker;
+	private String companyDesc;
 	public Shares(int shareId, String tickker, String companyDesc) {
 		super();
 		this.shareId = shareId;
@@ -21,19 +21,26 @@ public class Shares {
 	public String getTickker() {
 		return tickker;
 	}
-	public void setTickker(String tickker) {
-		this.tickker = tickker;
-	}
 	public String getCompanyDesc() {
 		return companyDesc;
 	}
 	public void setCompanyDesc(String companyDesc) {
 		this.companyDesc = companyDesc;
 	}
+
+	@Override
+	public boolean equals(Object obj) {
+		Shares compareObj = (Shares) obj;
+		return compareObj.shareId == this.shareId && compareObj.tickker.equals(this.tickker);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(shareId, tickker);
+	}
+
 	@Override
 	public String toString() {
 		return "Shares [shareId=" + shareId + ", tickker=" + tickker + ", companyDesc=" + companyDesc + "]";
 	}
-	
-	
 }
